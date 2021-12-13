e329d95d92c548625c603e0d0e4d90e07716471f
Alon <gabai.alon@gmail.com>
2021-12-13 18:18:53 +0200

    First upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+
+@app.route('/')
+def calculator_home_page_str():
+    return "Welcome to the greatest calculator ever !"
+
+
+@app.route('/add')
+def add():
+    first = int(request.args.get("first"))
+    second = int(request.args.get("second"))
+    res = first + second
+    return f'{first} + {second} = {res}'
+
+
+if __name__ == '__main__':
+    print(os.environ.get('PORT'))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT')))
